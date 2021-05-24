e8c18a2cc0cc681e70f47161acb554284e4727d8
melissateozuki <melissateozuki@gmail.com>
2021-05-24 23:14:39 +0800

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  resources :account_activations, only: [:edit]
   resources :users
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
